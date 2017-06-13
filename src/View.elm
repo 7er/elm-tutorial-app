b2e8467456cf32b82e0975319e27da7f61c1e9e0
Syver Enstad <syver.enstad@sparebank1.no>
2017-06-13 19:54:40 +0200

    Routing and Edit page

--- src/View.elm
+++ src/View.elm
@@ -2,8 +2,10 @@ module View exposing (..)
 
 import Html exposing (Html, div, text)
 import Msgs exposing (Msg)
-import Models exposing (Model)
+import Models exposing (Model, PlayerId)
+import Players.Edit
 import Players.List
+import RemoteData
 
 
 view : Model -> Html Msg
@@ -12,5 +14,38 @@ view model =
         [ page model ]
 
 page : Model -> Html Msg
-page model = 
-     Players.List.view model.players
+page model =
+  case model.route of
+    Models.PlayersRoute ->
+      Players.List.view model.players
+    Models.PlayerRoute id ->
+      playerEditPage model id
+    Models.NotFoundRoute ->
+      notFoundView
+
+
+playerEditPage : Model -> PlayerId -> Html Msg
+playerEditPage model playerId =
+  case model.players of
+      RemoteData.NotAsked -> text ""
+      RemoteData.Loading -> text "Loading ..."
+      RemoteData.Failure err -> text (toString err)
+      RemoteData.Success players ->
+          let
+              maybePlayer =
+                  players
+                      |> List.filter (\player -> player.id == playerId)
+                      |> List.head
+          in
+              case maybePlayer of
+                  Just player ->
+                      Players.Edit.view player
+                  Nothing ->
+                      notFoundView
+
+
+notFoundView : Html Msg
+notFoundView =
+    div [] [
+      text "Not found"
+    ]
